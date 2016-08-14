b899c74bc17809e9024cf9d818e8a3e34e4abf1f
Sebastien Le Marchand <sebastien.lemarchand@gmail.com>
2016-08-14 22:37:29 +0200

    Direct URL access to the raw editor, with groupId and articleId in query string

---
+++ docroot/WEB-INF/src/com/slemarchand/webcontentraweditor/hook/action/WebContentRawEditorAction.java
@@ -0,0 +1,58 @@
+package com.slemarchand.webcontentraweditor.hook.action;
+
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletURLFactoryUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.service.JournalArticleServiceUtil;
+
+import javax.portlet.PortletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebContentRawEditorAction extends BaseStrutsAction {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		String namespace;
+		
+		String portletId = request.getParameter("p_p_id");
+		
+		if(Validator.isNotNull(portletId)) {
+			namespace = StringPool.UNDERLINE + portletId + StringPool.UNDERLINE;
+		} else {
+			namespace = StringPool.BLANK;
+		}
+		
+		long companyId = PortalUtil.getCompanyId(request);
+		
+		long groupId = ParamUtil.getLong(request, namespace + "groupId");
+		
+		String articleId = request.getParameter(namespace + "articleId");
+		
+		JournalArticle article = JournalArticleServiceUtil.getLatestArticle(
+				groupId, articleId, WorkflowConstants.STATUS_ANY);
+				
+		long id = article.getId();
+				
+		LiferayPortletURL url = PortletURLFactoryUtil.create(
+				request, "1_WAR_webcontentraweditorportlet", PortalUtil.getControlPanelPlid(companyId),
+				PortletRequest.RENDER_PHASE);
+		
+		url.setWindowState(LiferayWindowState.POP_UP);
+		
+		url.setParameter("id", Long.toString(id));
+		
+		response.sendRedirect(url.toString());
+		
+		return StringPool.BLANK;
+	}
+}
